df7a2a2dcfc0958bb5ef4af4d1fd27eec422525a
MrSpud001 <ethanashaw@gmail.com>
2026-07-04 12:05:25 -0600

    w1 assignment patch 1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,5 @@ const copydate = today.getFullYear();
 document.getElementById("currentyear").innerHTML = copydate;
 document.getElementById("lastModified").innerHTML =
   `Last modified: ${modified}`;
+
+//   slap some ham and cheese on it and call it a sandwich, bubba
